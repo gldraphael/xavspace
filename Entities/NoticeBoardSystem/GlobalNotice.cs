2b0884ca7900749f3fe1e23994c82d5ab3285bda
Joshua Dias <joshuadias@live.com>
2015-01-07 18:28:02 +0000

    Imported from Changeset 101

---
+++ Entities/NoticeBoardSystem/GlobalNotice.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace XavSpace.Entities.NoticeBoardSystem
+{
+    /// <summary>
+    /// Represents a notice
+    /// ! Temporary class - to be removed 
+    /// </summary>
+    public class GlobalNotice
+    {
+        [Key]
+        public string GlobalNoticeId { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        public string Title { get; set; }
+
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? Date { get; set; }
+
+        [NotMapped]
+        [DataType(DataType.DateTime)]
+        public DateTime? DateWithTime { get { return Date; } }
+
+        public GlobalNotice()
+        {
+            GlobalNoticeId = Guid.NewGuid().ToString();
+            Date = DateTime.Now;
+            
+        }
+    }
+
+    /// <summary>
+    /// Represents a database connection
+    /// ! Temporary class - to be removed later
+    /// </summary>
+    public class GlobalNoticeDbContext : DbContext 
+    {
+        public DbSet<GlobalNotice> Notices { get; set; }
+
+        public GlobalNoticeDbContext()
+            : base("SEProj")
+        { }
+    }
+}
---
+++ XavSpace.Website/Extensions/HtmlLayoutExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace XavSpace.Website.Extensions
+{
+    public static class HtmlLayoutExtension
+    {
+        public static bool IsAboutPage(this HtmlHelper html, HttpContextBase context)
+        {
+            return context.Request.RequestContext.RouteData.Values["controller"].ToString() == "About" && context.Request.RequestContext.RouteData.Values["action"].ToString() == "Index";
+        }
+    }
+}
