9ad99823e382c357dd18aa3dcc38a9e187071f52
Joshua Dias <joshuadias@live.com>
2015-02-09 20:33:32 +0000

    WYSIWYG done Imported from Changeset 133

--- XavSpace.Website/ViewModels/Notices/PendingNoticeViewModel.cs
+++ XavSpace.Website/ViewModels/Notices/PendingNoticeViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 using XavSpace.Entities.Data;
 
@@ -25,6 +26,8 @@ namespace XavSpace.Website.ViewModels.Notices
         /// <summary>
         /// Description of the notice
         /// </summary>
+        /// 
+        [AllowHtml]
         [DataType(DataType.MultilineText)]
         public string Description { get; set; }
 
