379b757defcc11a5fbd9a3b99c2efc194b8ffbfc
Galdin Raphael <gldraphael@gmail.com>
2015-04-07 16:52:25 +0000

    Updating code Imported from Changeset 188

--- XavSpace.DataAccess/DbContexts/ApplicationDbContext.cs
+++ XavSpace.DataAccess/DbContexts/ApplicationDbContext.cs
@@ -7,7 +7,6 @@ using System.Threading.Tasks;
 
 using Microsoft.AspNet.Identity.EntityFramework;
 
-using XavSpace.DataAccess.DbInitializers;
 using XavSpace.Entities.Data;
 using XavSpace.Entities.Users;
 using XavSpace.Entities.Relationships;
@@ -21,43 +20,17 @@ namespace XavSpace.DataAccess.DbContexts
             : base("XSLocal", throwIfV1Schema: false)
         {
         }
-
-        //static ApplicationDbContext()
-        //{
-        //    // Set the database intializer which is run once during application start
-        //    // This seeds the database with admin user credentials and admin role
-        //    Database.SetInitializer<ApplicationDbContext>(new ApplicationDbInitializer());
-        //}
-
+        
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
         }
-
-        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        //{
-        //    base.OnModelCreating(modelBuilder);
-
-        //    //modelBuilder.Entity<IdentityUser>().ToTable("Users");
-        //    //modelBuilder.Entity<IdentityRole>().ToTable("Roles");
-        //    //modelBuilder.Entity<IdentityUserRole>().ToTable("UserRoles");
-        //    //modelBuilder.Entity<IdentityUserLogin>().ToTable("UserLogins");
-        //    //modelBuilder.Entity<IdentityUserClaim>().ToTable("UserClaims");
-
-        //    modelBuilder.Entity<IdentityUser>().ToTable("MyUsers").Property(p => p.Id).HasColumnName("UserId");
-        //    modelBuilder.Entity<ApplicationUser>().ToTable("MyUsers").Property(p => p.Id).HasColumnName("UserId");
-        //    modelBuilder.Entity<IdentityUserRole>().ToTable("MyUserRoles");
-        //    modelBuilder.Entity<IdentityUserLogin>().ToTable("MyUserLogins");
-        //    modelBuilder.Entity<IdentityUserClaim>().ToTable("MyUserClaims");
-        //    modelBuilder.Entity<IdentityRole>().ToTable("MyRoles");
-        //}
 
         // Data
         public DbSet<NoticeBoard> NoticeBoards { get; set; }
         public DbSet<Notice> Notices { get; set; }
 
         public DbSet<ErrorLog> ErrorLogs { get; set; }
-
 
         // Relationship mappings
         public DbSet<UserNoticeBoardFollow> UserBoardFollowingRelationship { get; set; }
--- XavSpace.Website/Controllers/NoticesController.cs
+++ XavSpace.Website/Controllers/NoticesController.cs
@@ -28,24 +28,22 @@ namespace XavSpace.Website.Controllers
                 var postedBy = await nm.PostedBy(noticeId);
                 var current = await User.Identity.GetApplicationUserAsync();
                 var notice = await nm.GetAsync(noticeId);
-                
 
                 if ((postedBy.Id == current.Id && notice.IsPendingApproval)
                     || await User.Identity.IsModeratorAsync())
                 {
                     var res = await nm.DeleteAsync(noticeId);
                     //if (res > 0) ;
-                        //return Json(JsonViewModel.Success);
-                       
+                    //  return Json(JsonViewModel.Success);
                 }
-               
+
             }
-           // return Json(JsonViewModel.Error);
+            // return Json(JsonViewModel.Error);
             return RedirectToAction("Index", "Home");
         }
 
         // POST: /Notices/Edit?noticeId=5
-        
+
         [HttpPost]
         public async Task<ActionResult> Edit(NoticeViewModel vm)
         {
@@ -55,7 +53,7 @@ namespace XavSpace.Website.Controllers
                 var current = await User.Identity.GetApplicationUserAsync();
                 var notice = await nm.GetAsync(vm.Id);
 
-                if(notice.Status == NoticeStatus.Approved)
+                if (notice.Status == NoticeStatus.Approved)
                     return Json(JsonViewModel.Error);
 
                 if ((postedBy.Id == current.Id && notice.IsPendingApproval)
@@ -68,132 +66,13 @@ namespace XavSpace.Website.Controllers
                     notice.Description = vm.Description;
                     notice.Status = NoticeStatus.PendingApproval;
                     var res = await nm.UpdateAsync(notice);
-                    
+
                     //if (res > 0) ;
-                    //return Json(JsonViewModel.Success);
+                    //  return Json(JsonViewModel.Success);
                 }
             }
             // return Json(JsonViewModel.Error);
             return RedirectToAction("Index", "MyPosts");
         }
-
-        #region Old COde
-        //private ApplicationDbContext db = new ApplicationDbContext();
-
-        //// GET: Notices
-        //public async Task<ActionResult> Index()
-        //{
-        //    var notices = db.Notices.Include(n => n.NoticeBoard);
-        //    return View(await notices.ToListAsync());
-        //}
-
-        //// GET: Notices/Details/5
-        //public async Task<ActionResult> Details(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    Notice notice = await db.Notices.FindAsync(id);
-        //    if (notice == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(notice);
-        //}
-
-        //// GET: Notices/Create
-        //public ActionResult Create()
-        //{
-        //    ViewBag.NoticeBoardId = new SelectList(db.NoticeBoards, "NoticeBoardId", "Title");
-        //    return View();
-        //}
-
-        //// POST: Notices/Create
-        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<ActionResult> Create([Bind(Include = "NoticeId,NoticeBoardId,Title,Description,HighPriority")] Notice notice)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.Notices.Add(notice);
-        //        await db.SaveChangesAsync();
-        //        return RedirectToAction("Index");
-        //    }
-
-        //    ViewBag.NoticeBoardId = new SelectList(db.NoticeBoards, "NoticeBoardId", "Title", notice.NoticeBoardId);
-        //    return View(notice);
-        //}
-
-        //// GET: Notices/Edit/5
-        //public async Task<ActionResult> Edit(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    Notice notice = await  db.Notices.FindAsync(id);
-        //    if (notice == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    ViewBag.NoticeBoardId = new SelectList(db.NoticeBoards, "NoticeBoardId", "Title", notice.NoticeBoardId);
-        //    return View(notice);
-        //}
-
-        //// POST: Notices/Edit/5
-        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<ActionResult> Edit([Bind(Include = "NoticeId,NoticeBoardId,Title,Description,DateCreated,DateApproved,HighPriority")] Notice notice)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.Entry(notice).State = EntityState.Modified;
-        //        await db.SaveChangesAsync();
-        //        return RedirectToAction("Index");
-        //    }
-        //    ViewBag.NoticeBoardId = new SelectList(db.NoticeBoards, "NoticeBoardId", "Title", notice.NoticeBoardId);
-        //    return View(notice);
-        //}
-
-        //// GET: Notices/Delete/5
-        //public async Task<ActionResult> Delete(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    Notice notice = await db.Notices.FindAsync(id);
-        //    if (notice == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(notice);
-        //}
-
-        //// POST: Notices/Delete/5
-        //[HttpPost, ActionName("Delete")]
-        //[ValidateAntiForgeryToken]
-        //public async Task<ActionResult> DeleteConfirmed(int id)
-        //{
-        //    Notice notice = await db.Notices.FindAsync(id);
-        //    db.Notices.Remove(notice);
-        //    await db.SaveChangesAsync();
-        //    return RedirectToAction("Index");
-        //}
-
-        //protected override void Dispose(bool disposing)
-        //{
-        //    if (disposing)
-        //    {
-        //        db.Dispose();
-        //    }
-        //    base.Dispose(disposing);
-        //}
-#endregion
     }
 }
